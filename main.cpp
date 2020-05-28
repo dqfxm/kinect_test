1b290150abc55b074c773521bf738d5507be221b
fxm-dell7740 <dqfxm@outlook.com>
2020-05-28 20:47:00 +0800

    c编译测试推测2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 //测试推送程序
-#include <iostream>
-#include <fstream>
-using namespace std;
-
-int main() {
-    cout << "main 测试Hello, world!!!!" << endl;
-     return 0;
+#include<stdio.h>
+int main()
+{
+printf("完美编译，这是c++编写的c语言，Hello World.\n");
+return 0;
 }
+
+// #include <iostream>
+// #include <fstream>
+// using namespace std;
+
+// int main() {
+//     cout << "main 测试Hello, world!!!!" << endl;
+//      return 0;
+// }
